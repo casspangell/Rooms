af033943df348bf5cf8b83ae8b78a32e1d211882
Cass Pangell <casspangell@Cass-MacBook-Pro.local>
2014-07-28 11:29:19 -0600

    Initial commit.

---
+++ Rooms/Rooms/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Rooms
+//
+//  Created by Cass Pangell on 7/28/14.
+//  Copyright (c) 2014 mondolabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Rooms/Rooms/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rooms
+//
+//  Created by Cass Pangell on 7/28/14.
+//  Copyright (c) 2014 mondolabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
